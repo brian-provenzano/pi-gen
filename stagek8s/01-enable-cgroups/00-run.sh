bb61f06d1763f151d945a9a62303c9b5fd344bd9
Brian Provenzano <bproven@gmail.com>
2019-10-22 09:13:43 -0600

    remove the stagek8s stage

--- stagek8s/01-enable-cgroups/00-run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -e
-
-# Enable cgroups in kernel on boot
-params="$(head -n1 ${ROOTFS_DIR}/boot/cmdline.txt) cgroup_enable=cpuset cgroup_memory=1 cgroup_enable=memory"
-echo $params | sudo tee "${ROOTFS_DIR}/boot/cmdline.txt"
