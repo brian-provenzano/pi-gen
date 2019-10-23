a51b92554755badeb14f2b5c04ecf92cd8bd8aa9
Brian Provenzano <bproven@gmail.com>
2019-10-22 22:23:50 -0600

    install new k8s tweaked versions instead of modding originals

--- stagek8s/04-enable-cgroups/00-run.sh
+++ stagek8s/04-enable-cgroups/00-run.sh
@@ -1,7 +1,3 @@
 #!/bin/bash -e
 
-on_chroot << EOF
-# Enable cgroups in kernel on boot
-params="$(head -n1 ${ROOTFS_DIR}/boot/cmdline.txt) cgroup_enable=cpuset cgroup_memory=1 cgroup_enable=memory"
-echo $params | sudo tee "${ROOTFS_DIR}/boot/cmdline.txt"
-EOF
+install -m 644 files/cmdline.txt "${ROOTFS_DIR}/boot/"
--- stagek8s/05-set-gpumem/00-run.sh
+++ stagek8s/05-set-gpumem/00-run.sh
@@ -1,6 +1,3 @@
 #!/bin/bash -e
 
-on_chroot << EOF
-# Set GPU mem to lowest value of 16MB since this is a k8s node
-echo -e "\ngpu_mem=16" >> "${ROOTFS_DIR}/boot/config.txt"
-EOF
+install -m 644 files/config.txt "${ROOTFS_DIR}/boot/"
