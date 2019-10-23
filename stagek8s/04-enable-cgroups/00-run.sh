75881c4a4df3acd82fc344c2da413916698c50b1
Brian Provenzano <bproven@gmail.com>
2019-10-23 13:12:17 -0600

    add debug to the file installs for confirm installation

--- stagek8s/04-enable-cgroups/00-run.sh
+++ stagek8s/04-enable-cgroups/00-run.sh
@@ -1,3 +1,7 @@
 #!/bin/bash -e
 
 install -m 644 files/cmdline.txt "${ROOTFS_DIR}/boot/"
+
+echo ">> CHECK IF INSTALLED NEW K3S CMDLINE.TXT KERNEL LINES"
+echo "${ROOTFS_DIR}"
+cat "${ROOTFS_DIR}/boot/cmdline.txt"
--- stagek8s/05-set-gpumem/00-run.sh
+++ stagek8s/05-set-gpumem/00-run.sh
@@ -1,3 +1,7 @@
 #!/bin/bash -e
 
 install -m 644 files/config.txt "${ROOTFS_DIR}/boot/"
+
+echo ">> CHECK IF INSTALLED NEW K3S CONFIG.TXT"
+echo "${ROOTFS_DIR}"
+cat "${ROOTFS_DIR}/boot/config.txt"
