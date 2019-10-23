17c0e65e8e45e343074a2073bd5cd8d2e9f19716
Brian Provenzano <bproven@gmail.com>
2019-10-23 17:20:11 -0600

    add static ip option; set env var in config to use

--- stagek8s/06-network-static/00-run.sh
+++ stagek8s/06-network-static/00-run.sh
@@ -1,5 +1,20 @@
 #!/bin/bash -e
 ##
 # Modify this if you wish static ip (set the STATIC_IP env var in config )
+# Currently this sets the eth0 and wlan (wifi interface) to same static IP (you will use one or the other usually)
 ##
-# Set GPU mem to lowest value of 16MB since this is a k8s node
+
+if [[ ! -z "${STATIC_IP}" ]]; then
+    tee -a /etc/dhcpcd.conf <<EOF
+# override: set static IP
+interface eth0
+static ip_address=10.0.0.200/24
+static routers=10.0.0.1
+static domain_name_servers=1.1.1.1 8.8.8.8
+
+interface wlan0
+static ip_address=10.0.0.200/24
+static routers=10.0.0.1
+static domain_name_servers=1.1.1.1 8.8.8.8
+EOF
+fi
