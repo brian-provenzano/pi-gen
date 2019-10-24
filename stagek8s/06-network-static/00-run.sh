004001777287655cfb765d6898db0213a8dcad65
Brian Provenzano <bproven@gmail.com>
2019-10-24 09:44:49 -0600

    fix static IP script -add some confirmations

--- stagek8s/06-network-static/00-run.sh
+++ stagek8s/06-network-static/00-run.sh
@@ -3,9 +3,10 @@
 # Modify this if you wish static ip (set the STATIC_IP env var in config )
 # Currently this sets the eth0 and wlan (wifi interface) to same static IP (you will use one or the other usually)
 ##
+echo ">> DO WE WANT STATIC IP SET? [${STATIC_IP}]"
 
 if [[ ! -z "${STATIC_IP}" ]]; then
-    tee -a "${ROOTFS_DIR}"/etc/dhcpcd.conf <<EOF
+    tee -a "${ROOTFS_DIR}/etc/dhcpcd.conf" <<EOF
 # override: set static IP
 interface eth0
 static ip_address=$ETH0_IPADDRESS
@@ -17,6 +18,7 @@ static ip_address=$WLAN_IPADDRESS
 static routers=$WLAN_GATEWAY
 static domain_name_servers=$WLAN_DNS
 EOF
+    echo ">> SET STATIC IPS ... DONE!"
 fi
 
 echo ">> CHECK IF MODDED DHCPCD.CONF"
